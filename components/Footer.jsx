541287de494956a8a54063f888fe6707804c8284
lokeshRedEye <lokeshcoder123@gmail.com>
2025-06-28 18:29:39 +0530

    gut

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = ({isDarkMode}) => {
     <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
         <p>© LokeshCoder  Full Stack Developer.</p>
         <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
-            <li><a target='_blank' href="https://github.com/lokeshRedEye">GitHub</a></li>
+            <li><a target='_blank' href="https://github.com/lokesh-a-dev">GitHub</a></li>
             <li><a target='_blank' href="https://linkedin.com/in/lokeshcoder123">LinkedIn</a></li>
             <li><a target='_blank' href="https://leetcode.com/u/lokeshcoder123/">LeetCode</a></li>
         </ul>
